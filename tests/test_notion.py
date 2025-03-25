159202ac717b7a1482ee11465b1e1d86087e65cc
Allie Lapinski <alex.lapinski@gmail.com>
2025-03-25 14:17:18 -0400

    Added simple tool to create, read, or update Notion

--- tests/test_notion.py
+++ tests/test_notion.py
@@ -7,5 +7,13 @@ def setup_function(function):
 
 def test_tool_retrive_page():
     page_id = "a19b316f0b8d4514b21b282430dbeac0"
+    formatted_page_id = "a19b316f-0b8d-4514-b21b-282430dbeac0"
     result = tool_retrieve_page(page_id)
-    assert result == None
+    
+    assert result is not None
+
+    object_type = result["object"]
+    object_id = result["id"]
+    
+    assert object_type == "page"
+    assert object_id == formatted_page_id
--- tools/tool_notion.py
+++ tools/tool_notion.py
@@ -10,6 +10,4 @@ def tool_retrieve_page(page_id:str):
     A tool to interact with the notion application, specifically fetch a page using it's id
     """
     notion = create_client()
-    #return notion.pages.retrieve(page_id=page_id)
-    return notion.search(query="vape store").get("results")
-    #return notion.users.list()
+    return notion.pages.retrieve(page_id=page_id)
